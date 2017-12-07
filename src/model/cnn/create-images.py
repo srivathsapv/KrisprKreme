7f390ff14e0846a5a7f1f99eaf2b8974fcec1a80
Srivathsa PV <pv.srivathsa@gmail.com>
2017-12-07 13:34:27 -0500

    More changes to CNN code

--- src/model/cnn/create-images.py
+++ src/model/cnn/create-images.py
@@ -46,31 +46,30 @@ def create_11x11_images():
         print('create image for sequence id={}'.format(row['id']))
         file_name = 'seq_{}.png'.format(row['id'])
         seq = row['sequence']
-        feature_img = np.ndarray((11, 11, 3))
+        feature_img = np.ndarray((32, 32, 3))
 
         trunc_score = int(row['score']*10)/float(10)
         class_label = int(float('%.1f' % trunc_score) * 10)
         two_class_label = 1 if class_label >= 5 else 0
 
-        full_file_name = 'data/bit-images/11x11/{}'.format(file_name)
+        full_file_name = 'data/bit-images/32x32/{}'.format(file_name)
         id_to_classes.append([full_file_name, two_class_label])
 
         feature_list = []
         for i, c in enumerate(seq):
             feature_list.extend(char_map[c])
 
-        feature_list.append(0)
-        reshaped_features = np.reshape(feature_list, (11, 11)) * 255
+        feature_list.extend([0] * 904)
+        reshaped_features = np.reshape(feature_list, (32, 32)) * 255
 
         feature_img[:, :, 0] = reshaped_features
         feature_img[:, :, 1] = reshaped_features
         feature_img[:, :, 2] = reshaped_features
 
-
         png.from_array((feature_img).astype(np.uint8), 'RGB').save(full_file_name)
 
-    train_class_file = open('data/img_11x11_train_classes.txt', 'w')
-    test_class_file = open('data/img_11x11_test_classes.txt', 'w')
+    train_class_file = open('data/img_32x32_train_classes.txt', 'w')
+    test_class_file = open('data/img_32x32_test_classes.txt', 'w')
 
     for class_label in id_to_classes[:5000]:
         train_class_file.write('{} {}\n'.format(class_label[0], class_label[1]))
